5cabbc65cfe0661a3fd45b32fbf14233605d3f7c
Eric Li <zihaol1@vt.edu>
2021-04-21 11:04:21 -0400

    Add ExpenseList.swift

---
+++ Monimize/ExpenseList.swift
@@ -0,0 +1,20 @@
+//
+//  ExpenseList.swift
+//  Monimize
+//
+//  Created by Eric Li on 4/21/21.
+//
+
+import SwiftUI
+
+struct ExpenseList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExpenseList_Previews: PreviewProvider {
+    static var previews: some View {
+        ExpenseList()
+    }
+}
