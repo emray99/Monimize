9daafd89c3575f746a1809ba68aeec13282334c8
Ray Liu <emray@vt.edu>
2021-04-25 09:02:28 -0400

    Part of the "Search for National Park" feature that I somehow didn't commit

---
+++ Monimize/Tab Views/ParkSearchResultsList.swift
@@ -0,0 +1,33 @@
+//
+//  ParkSearchResultsList.swift
+//  Monimize
+//
+//  Created by Ray Liu on 4/25/21.
+//
+
+import SwiftUI
+
+struct ParkSearchResutsList: View {
+    
+    var body: some View {
+       
+            List {
+                ForEach(parkResultsList) { aPark in
+
+                    NavigationLink(destination: ParkDetails(park: aPark)) {
+
+                        ParkItem(park: aPark)
+
+                    }
+                }
+            }   // End of List
+            .navigationBarTitle(Text("Search Results"), displayMode: .inline)
+       
+        .customNavigationViewStyle()      // Given in NavigationStyle.swift
+
+    }   // End of body
+}
+
+
+ 
+
