19400c04487d87bf2dc5594b673205f27eb5e95d
Ray Liu <emray@vt.edu>
2021-04-26 17:38:32 -0400

    Added Gadgets

---
+++ Monimize/Tab Views/CurrencyConverterPage.swift
@@ -0,0 +1,20 @@
+//
+//  CurrencyConverterPage.swift
+//  Monimize
+//
+//  Created by Ray Liu on 4/26/21.
+//
+
+import SwiftUI
+
+struct CurrencyConverterPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CurrencyConverterPage_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrencyConverterPage()
+    }
+}
---
+++ Monimize/Tab Views/QRCodePage.swift
@@ -0,0 +1,20 @@
+//
+//  QRCodePage.swift
+//  Monimize
+//
+//  Created by Ray Liu on 4/26/21.
+//
+
+import SwiftUI
+
+struct QRCodePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QRCodePage_Previews: PreviewProvider {
+    static var previews: some View {
+        QRCodePage()
+    }
+}
