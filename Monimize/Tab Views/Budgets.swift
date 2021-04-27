c9808406ecc5e696d965c0f8e790bcef10698cba
zihaol1 <64662844+zihaol1@users.noreply.github.com>
2021-04-27 00:30:38 -0400

    Modified Budget view

--- Monimize/Tab Views/Budgets.swift
+++ Monimize/Tab Views/Budgets.swift
@@ -8,8 +8,61 @@
 import SwiftUI
 
 struct Budgets: View {
+    @State private var showBudgetSettings = false
+    @State private var flag = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if flag != true {
+            VStack {
+                Text("You should create budgets in order to kepp track of your expenses in specific areas like Car Expenses, House expenses, Food expenses, and so on. Click the New Budget button to start.")
+                    .fontWeight(.light)
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(6)
+                    .padding()
+                
+                Button(action: {
+                    self.showBudgetSettings.toggle()
+                }) {
+                    HStack {
+                        Image(systemName: "plus")
+                            //.frame(width: 360)
+                            .foregroundColor(.blue)
+                            
+                        Text("New Budget")
+                            
+                            .foregroundColor(.blue)
+                    }
+                    .frame(width: 320, height: 50)
+                    .background(Color.gray.opacity(0.1).edgesIgnoringSafeArea(.all))
+
+                }
+                .cornerRadius(24.0)
+                .actionSheet(isPresented: $showBudgetSettings, content: { budgetSettings })
+            }
+            
+            
+        } else {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+       
+    }
+    
+    var budgetSettings: ActionSheet {
+        ActionSheet(title: Text("Budget Settings"),
+                    //message: Text("Select Map Type"),
+                    buttons: [
+                        .default(Text("Expense")) {
+                            self.flag = true
+                            self.showBudgetSettings = false
+                        },
+                        .default(Text("Income")) {
+                            //self.selectedMapType = MKMapType.satellite
+                            self.showBudgetSettings = false
+                        },
+                        
+                        .cancel() {
+                            self.showBudgetSettings = false
+                        }
+                    ])
     }
 }
 
