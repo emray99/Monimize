de1af4bcb1c0c38bbfeef2acf0725fb116e6bd36
zihaol1 <64662844+zihaol1@users.noreply.github.com>
2021-04-27 01:10:57 -0400

    Split Budget view into 2 views

---
+++ Monimize/Tab Views/AddBudget.swift
@@ -0,0 +1,20 @@
+//
+//  AddBudget.swift
+//  Monimize
+//
+//  Created by Eric Li on 4/27/21.
+//
+
+import SwiftUI
+
+struct AddBudget: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddBudget_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBudget()
+    }
+}
